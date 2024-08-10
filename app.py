aa0fc4b67272c0645b99c75c4f10d4701c181657
GIANLOZ90 <Gianloz@hotmail.it>
2024-08-10 16:47:28 +0200

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,3 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Crea le tabelle nel database
     app.run(debug=True)
-
-commit -m "Corretto il contenuto di app.py
