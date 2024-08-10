42f2037a688192261f506612ddb0f84ee5727d30
GIANLOZ90 <Gianloz@hotmail.it>
2024-08-10 21:24:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -81,3 +81,13 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Crea le tabelle nel database
     app.run(debug=True)
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+# Definisci qui le tue rotte e logiche
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
